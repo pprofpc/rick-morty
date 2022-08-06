b25ca7a5ac6b9f0b2bf5fc2080c3be69f5958403
Pablo CANEPA <pprofpc@gmail.com>
2022-08-06 19:19:15 -0300

    Add search button in home

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import imageRickMorty from "./img/rick-morty.png" ;
 import './App.css';
 
 function App() {
+  const reqApi = () => {
+    console.log("Clicking");
+  }
   return (
     <div className="App">
       <header className="App-header">
         <h1 className='title'>Rick & Morty</h1>
         <img src={imageRickMorty} alt="Rick & Morty"  className='img-home' />
+        <button onClick={reqApi} className="btn-search">Buscar Personaje</button>
       </header>
     </div>
   );
