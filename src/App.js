9db20d6fae91d5fb0358058f6a1dc061490bbeaa
Pablo CANEPA <pprofpc@gmail.com>
2022-08-06 21:20:24 -0300

    Consultamos API. Componente Characters

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import imageRickMorty from "./img/rick-morty.png" ;
 import './App.css';
-//import { useState } from "react";
+import { useState } from "react";
+import Characters from "./components/Characters";
 
 function App() {
 
-
+  const [characters, setCharacters] = useState(null);
+  
   const reqApi = async () => {
-    //const [characters, setCharacters] = useState(null);
-
     const api = await fetch('https://rickandmortyapi.com/api/character');
     const characterApi = await api.json();
-    console.log(characterApi);
-  }
+
+    setCharacters(characterApi.results);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1 className='title'>Rick & Morty</h1>
+
+
+
         <img src={imageRickMorty} alt="Rick & Morty"  className='img-home' />
         <button onClick={reqApi} className="btn-search">Buscar Personaje</button>
       </header>
---
+++ src/components/Characters.js
@@ -0,0 +1,7 @@
+export default function Characters(){
+    return (
+    <div>
+        <h1>Desde characters JS</h1>
+    </div>
+    );
+}
