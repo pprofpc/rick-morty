1b6941cb3936a9c7db3e33844ab7c5d1f1cacd3d
Pablo CANEPA <pprofpc@gmail.com>
2022-08-06 22:09:14 -0300

    Contenedor de personajes finalizada

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,8 +1,45 @@
-export default function Characters(props){
-    console.log('Componente: '+props)
+export default function Characters(props) {
+    const { characters } = props;
+
     return (
-    <div>
-        <h1>Desde characters JS</h1>
-    </div>
+        <div className="characters">
+            <h1>Personajes</h1>
+            <span className="back-home">Volver</span>
+            <div className="container-characters">
+                {characters.map((character, index) => (
+                    <div className="character-container" key={index}>
+                        <div>
+                            <img src={character.image} alt={character.name} />
+                        </div>
+                        <div>
+                            <h3>{character.name}</h3>
+
+                            <h6>
+                                {character.status === "Alive" ? (
+                                    <>
+                                        <span className="alive" />
+                                        Vivo
+                                    </>
+                                ) : (
+                                    <>
+                                        <span className="dead" />
+                                        Muerto
+                                    </>
+                                )}
+                            </h6>
+                            <p>
+                                <span className="text-grey">Episodios: </span>
+                                <span>{character.episode.length}</span>
+                            </p>
+                            <p>
+                                <span className="text-grey">Especie: </span>
+                                <span>{character.species}</span>
+                            </p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <span className="back-home">Volver</span>
+        </div>
     );
 }
